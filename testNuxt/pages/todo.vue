da943ab500f0324f634d0c24cfb2007a78d5428c
ErdniSamkhaev <samhaev.e@yandex.ru>
2024-11-28 13:19:12 +0300

    sort

--- testNuxt/pages/todo.vue
+++ testNuxt/pages/todo.vue
@@ -46,6 +46,14 @@
       >
         Невыполненные
       </button>
+      <!-- Сортировка по дате -->
+      <select
+        v-model="sortOrder"
+        class="px-4 py-2 rounded-lg shadow bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 transition-transform hover:scale-105"
+      >
+        <option value="newest">Сначала новые</option>
+        <option value="oldest">Сначала старые</option>
+      </select>
     </div>
 
     <!-- Добавляем новую задачу -->
