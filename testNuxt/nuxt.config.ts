162ca9a625b7078011f29beebb41becf5170d58b
ErdniSamkhaev <samhaev.e@yandex.ru>
2024-12-01 07:14:53 +0300

    config.ts

--- testNuxt/nuxt.config.ts
+++ testNuxt/nuxt.config.ts
@@ -7,71 +7,56 @@ export default defineNuxtConfig({
       description: "Простое приложение для управления задачами",
       theme_color: "#ffffff",
       icons: [
-        {
-          src: "/icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-        },
-        {
-          src: "/icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "/icon-128x128.png",
-          sizes: "128x128",
-          type: "image/png",
-        },
-        {
-          src: "/icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "/icon-256x256.png",
-          sizes: "256x256",
-          type: "image/png",
-        },
-        {
-          src: "/icon-384x384.png",
-          sizes: "384x384",
-          type: "image/png",
-        },
-        {
-          src: "/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
+        { src: "/icon-72x72.png", sizes: "72x72", type: "image/png" },
+        { src: "/icon-96x96.png", sizes: "96x96", type: "image/png" },
+        { src: "/icon-128x128.png", sizes: "128x128", type: "image/png" },
+        { src: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+        { src: "/icon-256x256.png", sizes: "256x256", type: "image/png" },
+        { src: "/icon-384x384.png", sizes: "384x384", type: "image/png" },
+        { src: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
       ],
     },
     workbox: {
-      globPatterns: ["**/*.{js,css,html,png,svg,ico}"], // Автокэширование файлов
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
       runtimeCaching: [
         {
-          urlPattern: /^https:\/\/train-nuxt\.vercel\.app\//, // URL приложения
-          handler: "NetworkFirst", // Приоритет сети
+          urlPattern: /^https:\/\/train-nuxt\.vercel\.app\/todo\/.*/, // Динамические маршруты
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "dynamic-routes",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 7 * 24 * 60 * 60,
+            },
+            networkTimeoutSeconds: 10,
+          },
+        },
+        {
+          urlPattern: /^https:\/\/train-nuxt\.vercel\.app\//,
+          handler: "NetworkFirst",
           options: {
             cacheName: "app-cache",
             expiration: {
-              maxEntries: 50, // Ограничение по количеству файлов
-              maxAgeSeconds: 24 * 60 * 60, // 1 день
+              maxEntries: 50,
+              maxAgeSeconds: 24 * 60 * 60,
             },
           },
         },
         {
-          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\//, // Google Fonts
+          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\//,
           handler: "CacheFirst",
           options: {
             cacheName: "google-fonts",
             expiration: {
               maxEntries: 20,
-              maxAgeSeconds: 365 * 24 * 60 * 60, // 1 год
+              maxAgeSeconds: 365 * 24 * 60 * 60,
             },
           },
         },
       ],
+      navigateFallback: null, // Отключить fallback
     },
-    registerType: "autoUpdate", // Автоматическое обновление Service Worker
+    registerType: "autoUpdate",
   },
   ssr: false,
   app: {
@@ -84,7 +69,7 @@ export default defineNuxtConfig({
       VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
     },
   },
-  css: ["~/assets/css/main.css"], // Подключаем стили
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
