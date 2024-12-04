99f6be0624e0511cc06732e5bed424c9eebd960c
ErdniSamkhaev <samhaev.e@yandex.ru>
2024-12-05 00:09:39 +0300

    middleware

--- testNuxt/middleware/auth.js
+++ testNuxt/middleware/auth.js
@@ -1,16 +1,22 @@
-export default defineNuxtRouteMiddleware(async () => {
-    const { $supabase } = useNuxtApp();
-    const { data: session } = await $supabase.auth.getSession();
-  
-    // Проверяем, есть ли активная сессия
-    if (!session?.session) {
-      const { error } = await $supabase.auth.refreshSession();
-      if (error) {
-        return navigateTo("/login");
-      }
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { $supabase } = useNuxtApp();
+  const { data: session } = await $supabase.auth.getSession();
+
+  // Разрешаем доступ к странице восстановления пароля без проверки сессии
+  if (to.path === "/reset-password") {
+    return;
+  }
+
+  // Если сессии нет, пытаемся обновить токен или перенаправляем на /login
+  if (!session?.session) {
+    const { error } = await $supabase.auth.refreshSession();
+    if (error) {
+      return navigateTo("/login");
     }
-    if (session && to.path !== "/reset-password") {
-      return navigateTo("/");
-    }
-  });
-  
+  }
+
+  // Если пользователь авторизован и пытается зайти на страницу входа или регистрации
+  if (session?.session && (to.path === "/login" || to.path === "/register")) {
+    return navigateTo("/");
+  }
+});
